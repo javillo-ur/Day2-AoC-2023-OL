7ba8141d28907122e2ad988670b3a41b3241d968
javillo-ur <55446633+javillo-ur@users.noreply.github.com>
2023-12-03 19:08:40 +0100

    True one line

--- Day2Part2/Program.cs
+++ Day2Part2/Program.cs
@@ -1,5 +1,1 @@
-﻿Console.WriteLine(File.ReadAllLines("input.txt").Select(j => j.Split(":")[1])
-    .Select(j => j.Split(";").SelectMany(r => r.Split(",")))
-    .Sum(j => j.Where(r => r.Split(" ").Any(t => t == "red")).Max(r => int.Parse(r.Split(" ").First(k => int.TryParse(k, out _)))) *
-              j.Where(r => r.Split(" ").Any(t => t == "blue")).Max(r => int.Parse(r.Split(" ").First(k => int.TryParse(k, out _)))) *
-              j.Where(r => r.Split(" ").Any(t => t == "green")).Max(r => int.Parse(r.Split(" ").First(k => int.TryParse(k, out _))))));
+﻿Console.WriteLine(File.ReadAllLines("input.txt").Select(j => j.Split(":")[1]).Select(j => j.Split(";").SelectMany(r => r.Split(","))).Sum(j => j.Where(r => r.Split(" ").Any(t => t == "red")).Max(r => int.Parse(r.Split(" ").First(k => int.TryParse(k, out _)))) *j.Where(r => r.Split(" ").Any(t => t == "blue")).Max(r => int.Parse(r.Split(" ").First(k => int.TryParse(k, out _)))) *j.Where(r => r.Split(" ").Any(t => t == "green")).Max(r => int.Parse(r.Split(" ").First(k => int.TryParse(k, out _))))));
